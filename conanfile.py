9d4db9769ed95e183789bbb189284060a878f465
SSE4 <tomskside@gmail.com>
2017-11-01 22:03:28 +0700

    - make ninja suitable for cross-platform development

--- conanfile.py
+++ conanfile.py
@@ -1,39 +1,38 @@
 from conans import ConanFile, tools
 import os
+import platform
+import stat
 
 
 class NinjaConan(ConanFile):
-    name = "ninja"
+    name = "ninja_installer"
     version = "1.8.2"
     license = "Apache 2.0"
     description = "Ninja is a small build system with a focus on speed"
     url = "https://github.com/SSE4/conan-ninja_installer"
     no_copy_source = True
-    settings = {"os"}
-    repository = "https://chromium.googlesource.com/chromium/tools/depot_tools.git"
 
-    def source(self):
-        if self.settings.os == "Windows":
-            platform = "win"
-        elif self.settings.os == "Linux":
-            platform = "linux"
-        elif self.settings.os == "Macos" or self.settings.os == "iOS":
-            platform = "mac"
-        else:
-            raise Exception("unsupported os %s" % self.settings.os)
-
-        archive_name = "ninja-%s.zip" % platform
-        url = "https://github.com/ninja-build/ninja/releases/download/v%s/%s" % (self.version, archive_name)
-        tools.download(url, archive_name, verify=True)
-        tools.unzip(archive_name)
-        os.unlink(archive_name)
+    def build(self):
+        for _platform in ["win", "linux", "mac"]:
+            os.makedirs(_platform)
+            with tools.chdir(_platform):
+                archive_name = "ninja-%s.zip" % _platform
+                url = "https://github.com/ninja-build/ninja/releases/download/v%s/%s" % (self.version, archive_name)
+                tools.download(url, archive_name, verify=True)
+                tools.unzip(archive_name)
+                os.unlink(archive_name)
 
     def package(self):
-        if self.settings.os == "Windows":
-            self.copy(pattern="ninja.exe", dst=".", src=".")
-        else:
-            self.copy(pattern="ninja", dst=".", src=".")
+        for _platform in ["win", "linux", "mac"]:
+            self.copy(pattern="ninja*", dst=_platform, src=_platform)
 
     def package_info(self):
-        bin_path = os.path.join(self.package_folder)
+        _platform = {'Darwin': 'mac', 'Linux': 'linux', 'Windows': 'win'}.get(platform.system())
+        bin_path = os.path.join(self.package_folder, _platform)
+        # ensure ninja is executable
+        if os.name == 'posix':
+            name = os.path.join(bin_path, 'ninja')
+            mode = os.stat(name).st_mode
+            mode |= stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
+            os.chmod(name, stat.S_IMODE(mode))
         self.env_info.path.append(bin_path)
