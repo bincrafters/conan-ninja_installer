73fabbf561fc2cfda41ad2775d6ea88bda24785d
SSE4 <tomskside@gmail.com>
2018-01-26 13:45:51 +0700

    - update from templates

--- build.py
+++ build.py
@@ -1,28 +1,11 @@
-from conan.packager import ConanMultiPackager, os, re
-import platform
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from bincrafters import build_template_default
 
 if __name__ == "__main__":
-    reponame_a = os.getenv("APPVEYOR_REPO_NAME","")
-    repobranch_a = os.getenv("APPVEYOR_REPO_BRANCH","")
 
-    reponame_t = os.getenv("TRAVIS_REPO_SLUG","")
-    repobranch_t = os.getenv("TRAVIS_BRANCH","")
+    builder = build_template_default.get_builder()
 
-    if reponame_t or reponame_a:
-        username, repo = reponame_a.split("/") if reponame_a else reponame_t.split("/")
-        username = 'bincrafters'
-        channel, version = repobranch_a.split("/") if repobranch_a else repobranch_t.split("/")
-
-        with open("conanfile.py", "r") as conanfile:
-            contents = conanfile.read()
-            name = re.search(r'name\s*=\s*"(\S*)"', contents).groups()[0]
-
-        os.environ["CONAN_USERNAME"] = username
-        os.environ["CONAN_CHANNEL"] = channel
-        os.environ["CONAN_REFERENCE"] = "{0}/{1}".format(name, version)
-        os.environ["CONAN_UPLOAD"]="https://api.bintray.com/conan/{0}/public-conan".format(username)
-        os.environ["CONAN_REMOTES"]="https://api.bintray.com/conan/conan-community/conan"
-
-    builder = ConanMultiPackager()
-    builder.add({}, {}, {}, {})
     builder.run()
