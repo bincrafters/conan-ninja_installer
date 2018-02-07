99e01a840a8decd85e2772a9708f2db2f5bc7664
SSE4 <tomskside@gmail.com>
2018-02-07 18:21:03 +0700

    - NameError: name 'filtered_builds' is not defined

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
+    filtered_builds = []
     builder = build_template_default.get_builder()
     for settings, options, env_vars, build_requires, reference in builder.items:
         settings["arch_build"] = settings["arch"]
