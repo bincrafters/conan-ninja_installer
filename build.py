88a61b010f96ce6259dcf58bcd3f78c3fad5cd56
SSE4 <tomskside@gmail.com>
2018-02-07 18:15:53 +0700

    - process arch_build

--- build.py
+++ build.py
@@ -7,5 +7,9 @@ from bincrafters import build_template_default
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
-
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        settings["arch_build"] = settings["arch"]
+        del settings["arch"]
+    filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
     builder.run()
