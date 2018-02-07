54c0acdc94ecf8eb3c1971173ed6854387c2211d
SSE4 <tomskside@gmail.com>
2018-02-07 09:43:33 +0700

    - fix path to ninja

--- conanfile.py
+++ conanfile.py
@@ -29,6 +29,6 @@ class NinjaConan(ConanFile):
     def package_info(self):
         # ensure ninja is executable
         if str(self.settings.os_build) in ['Linux', 'Macosx']:
-            name = 'ninja'
+            name = os.path.join(self.package_folder, 'bin', 'ninja')
             os.chmod(name, os.stat(name).st_mode | 0o111)
         self.env_info.PATH.append(os.path.join(self.package_folder, 'bin'))
