f01050f77de6f92e0efab8333227d9054adcd153
Jerry W <jerrywiltse@gmail.com>
2018-02-14 12:28:58 -0500

    switch arch to arch_build in build.py

--- build.py
+++ build.py
@@ -9,6 +9,6 @@ from bincrafters import build_shared
 if __name__ == "__main__":
 
     builder = build_template_installer.get_builder()
-    builder.add({"os" : build_shared.get_os(), "arch" : "x86"}, {}, {}, {})
-    builder.add({"os" : build_shared.get_os(), "arch" : "x86_64"}, {}, {}, {})
+    builder.add({"os" : build_shared.get_os(), "arch_build" : "x86"}, {}, {}, {})
+    builder.add({"os" : build_shared.get_os(), "arch_build" : "x86_64"}, {}, {}, {})
     builder.run()
