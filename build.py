8083e4b07a09428b2b4de33482f2f26108320167
SSE4 <tomskside@gmail.com>
2018-11-19 22:43:52 +0700

    - specify full option name
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     arch = os.environ["ARCH"]
     builder = build_template_installer.get_builder()
     builder.add({"os": build_shared.get_os(), "arch_build": arch}, {}, {}, {})
-    builder.add({"os": build_shared.get_os(), "arch_build": arch}, {"distribution": "kitware"}, {}, {})
+    builder.add({"os": build_shared.get_os(), "arch_build": arch}, {"ninja_installer:distribution": "kitware"}, {}, {})
     builder.run()
