df971c94294d5145a1b6616e8fcf9d4bbc70c415
SSE4 <tomskside@gmail.com>
2018-11-21 22:23:15 +0700

    - remove distribution option
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -11,5 +11,4 @@ if __name__ == "__main__":
     arch = os.environ["ARCH"]
     builder = build_template_installer.get_builder()
     builder.add({"os": build_shared.get_os(), "arch_build": arch}, {}, {}, {})
-    builder.add({"os": build_shared.get_os(), "arch_build": arch}, {"ninja_installer:distribution": "kitware"}, {}, {})
     builder.run()
